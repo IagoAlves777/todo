f490196d1f3a10a7f7f45a2f95660cc1eace7d54
Iago da Silva Alves <53544551+IagoAlves777@users.noreply.github.com>
2022-01-04 10:54:31 -0300

    Adciona tarefa e marca ela como concluida

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,46 @@ import {useState} from 'react';
 import * as C from './App.styles'
 import {Item} from './types/item'
 import {ListItem} from './components/ListItem'
+import {AddArea} from './components/Addarea'
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    {id:1, name: 'teste', done: false}, 
-    {id:1, name: 'teste2', done: false}, 
+    {id:1, name: 'Criação dos usuario do contas médicas', done: false}, 
+    {id:2, name: 'Terminar o projeto em react', done: true}, 
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length+1,
+      name: taskName,
+      done: false,
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange  = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList){
+      if(newList[i].id === id){
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+  
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+        <AddArea onEnter={handleAddTask}></AddArea>
+
         {list.map((item, index) =>(
-          <ListItem key={index} item={item}/>
+          <ListItem 
+            key={index} 
+            item={item}
+            onChange={handleTaskChange}  
+          />
         ))}
       </C.Area>
     </C.Container>
